4a8d3366864c0c3822f69d5960c05408e1a58b03
aafsari <amirafsari99@yahoo.com>
2023-01-31 17:11:55 +0330

    add rectangle function implementations

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -2,10 +2,12 @@ public class Rectangle {
     int width;
     int height;
     public Rectangle(int width, int height){
+        this.width = width;
+        this.height = height;
     }
 
     public int computeArea(){
-        return 0;
+        return this.width * this.height;
     }
 
 }
